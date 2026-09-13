a2a44fc66d048f58c1eb56e2bb6ac6b241e7ca6a
Julian Passebecq <64646018+julian-passebecq@users.noreply.github.com>
2026-09-13 18:44:07 +0200

    Switch app to format-only V9 board

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './AppV7'
+import App from './AppV9'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
