e099d24dbf318faa2152363a3077ff2f4f11b799
Julian Passebecq <64646018+julian-passebecq@users.noreply.github.com>
2026-09-13 19:29:37 +0200

    Activate refined ball-loss strategy UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './AppV12'
+import App from './AppV13'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
