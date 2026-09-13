1352312e4dec69bc1b7e816b86cbe7905685beac
Julian Passebecq <64646018+julian-passebecq@users.noreply.github.com>
2026-09-13 21:50:09 +0200

    Use final orientation-aware board shell

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './AppBoard.jsx'
-
-// Default the pitch to horizontal for new visitors. A manual choice remains remembered.
-try {
-  const viewKey = 'fornebu-pitch-view'
-  if (!window.localStorage.getItem(viewKey)) window.localStorage.setItem(viewKey, 'horizontal')
-} catch {
-  // Storage may be disabled; AppBoard will still render normally.
-}
+import App from './AppV16.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
