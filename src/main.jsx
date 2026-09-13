ecaf5d65d2411ead43ae895747558f77ddd98ce9
Julian Passebecq <64646018+julian-passebecq@users.noreply.github.com>
2026-09-13 18:15:14 +0200

    Switch app entry to V6 coach board

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './AppV5'
+import App from './AppV6'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
