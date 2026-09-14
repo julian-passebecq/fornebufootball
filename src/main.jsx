a7f610c8f2a0e774df84782b2b6eb414aa506b3b
Julian Passebecq <64646018+julian-passebecq@users.noreply.github.com>
2026-09-14 13:10:34 +0200

    Use refined selected player presentation

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './AppV16.jsx'
+import App from './AppV17.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
